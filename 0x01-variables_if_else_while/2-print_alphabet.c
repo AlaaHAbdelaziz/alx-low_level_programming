02701b8c990a7bbde132b78f4544fca97fad4eb0
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-14 14:46:22 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char letter;
-	for (letter = 'a'; letter <= 'z'; letter ++) {
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	}
 	putchar('\n');
 	return (0);
 
