9e9e78ef44a879e83f9c93994c50830d6f9b8899
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-14 07:45:02 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+sizeof(char);
+sizeof(int);
+sizeof(long int);
+sizeof(long long);
+sizeof(float);
+}
