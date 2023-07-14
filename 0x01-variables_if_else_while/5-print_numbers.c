500c55018dbbd75465b0442e3d159346dd790113
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-14 15:22:10 -0700

    4print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Discription: alphabet task
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number;
+
+for (number = 0; number < 10; number++)
+printf("%d\n" , number)
+return (0);
+}
