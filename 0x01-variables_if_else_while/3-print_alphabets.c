3843d8ef4195cfa5d42a1ad7c699c4798d26f038
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-14 14:54:28 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Discription: alphabet task
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char letter;
+
+        for (letter = 'a'; letter <= 'z'; letter++)
+                putchar(letter);
+        putchar('\n');
+        return (0);
+	
+        for (letter = 'A'; letter <= 'Z'; letter++)
+                putchar(letter);
+        putchar('\n');
+        return (0);
+}
