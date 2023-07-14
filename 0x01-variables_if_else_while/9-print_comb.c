5beb296fed903731ac811fb537ea927474aa9244
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-14 16:08:35 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Discription: alphabet task
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+putchar(num + '0');
+if (num != 9)
+{
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
