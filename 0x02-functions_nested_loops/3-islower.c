10aeef297dd35eed0ee2303048528000a0ca6115
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-18 19:10:17 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,15 @@
 /**
  * _islower - checks if the letter is lowercase
  *
+ * @c character to be checked
+ *
  * description: if lowercase or uppercase
  *
  * return: 1 for lowercase and 0 for otherwise
  */
 int _islower(int c)
 {
-if (c >= 'a' && c >= 'z')
+if (c >= 97 && c >= 122)
 {
 return (1);
 }
