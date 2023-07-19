71db76acab20257dbdc2df6b588f3181603fc1cc
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-18 18:24:11 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints alphabet 
+ * print_alphabet - prints alphabet
  * description: print_alphabet is a function for printing alphabet in lowercase
  * Return: void
  */
