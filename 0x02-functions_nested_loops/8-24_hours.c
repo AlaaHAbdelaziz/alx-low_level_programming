5da982f4347473e47970713a4351415a5074bbb1
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-19 07:50:13 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,17 +9,26 @@
  */
 void jack_bauer(void)
 {
-int hour, min;
-for (hour = 0; hour <= 24; hour++)
+int a, b, c, d;
+for (a = 0; a <= 2; a++)
 {
-for (min = 0; min <= 59; min++)
+for (b = 0; b <= 9; b++)
 {
-_putchar((hour / 10) + '0');
-_putchar((hour % 10) + '0');
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+{
+for (c = 0; c <= 5; c++)
+{
+for (d = 0; d <= 9; d++)
+{
+_putchar(a + '0');
+_putchar(b + '0');
 _putchar(':');
-_putchar((min / 10) + '0');
-_putchar((min % 10) + '0');
+_putchar(c + '0');
+_putchar(d + '0');
 _putchar('\n');
+}
+}
+}
 }
 }
 }
