95948ea653585803a796298f4ec785cf0b4fd686
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-19 07:39:03 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,8 +14,12 @@ for (hour = 0; hour <= 24; hour++)
 {
 for (min = 0; min <= 59; min++)
 {
-_putchar(hour);
-_putchar(min);
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar('\n');
 }
 }
 }
