bf87cf401aaafdedca2f0ee68cc9ed9d0afc94ea
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-19 08:35:02 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,19 +3,39 @@
 /**
  * times_table - 9 times table
  *
- * @n: character to be checked
- *
  * Discription: prints 9 times table
  *
  * Return: 9 times table, starting with 0
  */
 void times_table(void)
 {
-int n, i, cal;
-i = 9;
-for (n = 0; n <= 9; n++)
+int x, y, z, u, d;
+
+for (x = 0; x <= 9; x++)
 {
-cal = n * i;
-_putchar(cal);
+for (y = 0; y <= 9; y++)
+{
+z = x * y;
+
+if (z > 9)
+{
+u = z % 10;
+d = (z - u) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(d + '0');
+_putchar(u + '0');
+}
+else
+{
+if (y != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');			}
+}
+_putchar('\n');
 }
 }
