befba7260577076c20886729290ee90dc8719634
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-20 14:06:19 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - prints straight line
+ *
+ * Return: straight line
+ */
+void print_line(int n)
+{
+if (n == 0)
+{
+putchar('\n');
+}
+else
+{
+putchar('_');
+}
+}
