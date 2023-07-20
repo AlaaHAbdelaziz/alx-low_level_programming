a12bc9c6815e9c0eb9a5dd9ea3dc0021f8231f8b
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-20 08:14:40 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - looks for uppercase
+ *
+ * @c: character to be checked
+ *
+ * Discription: checks if c is an uppercase
+ *
+ * Return: 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+while (C <= 'A' || c >= 'Z')
+{
+_putchar(1);
+}
+return (0);
+}
