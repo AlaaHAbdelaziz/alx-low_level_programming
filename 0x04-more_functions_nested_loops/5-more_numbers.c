aab71a173ef88c54c86823b064b654655bf328f7
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-20 13:33:51 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,6 @@
 
 /**
  * more_numbers - prints more numbers
- *
  * Return:numbers from 0 to 9 14 times
  */
 void more_numbers(void)
@@ -13,6 +12,6 @@ for (j = 0; j < 14; j++)
 {
 for (i = 0; i < 10; i++)
 putchar(i + '0');
-}
 putchar('\n');
+}
 }
