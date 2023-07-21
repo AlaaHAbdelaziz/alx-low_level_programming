a55cfafa21497344c5b5ccfc872c34349bdc3a6a
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-20 19:31:28 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - print square
+ * @size: size of the square
+ * Return: nothing
+ */
+void print_square(int size)
+{
+int i, j;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < i; j++)
+putchar('#');
+putchar('\n');
+}
+}
+else
+{
+putchar('\n');
+}
+}
