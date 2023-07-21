25a44ff3e184bf6df77a9a52eec1003407721369
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-20 19:38:19 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints fizz bizz numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		(i % 3 == 0) ? printf("Fizz") : 0;
+		(i % 5 == 0) ? printf("Buzz") : 0;
+		(i % 3 != 0 && i % 5 != 0) ? printf("%d", i) : 0;
+		(i != 100) ? printf(" ") : 0;
+	}
+	printf("\n");
+	return (0);
+}
