ee4f15542376656464c28bde8c83fa63e42b1228
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-20 19:34:12 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,17 +9,16 @@
 void print_square(int size)
 {
 int i, j;
+
 if (size > 0)
 {
 for (i = 0; i < size; i++)
 {
-for (j = 0; j < i; j++)
+for (j = 0; j < size; j++)
 putchar('#');
 putchar('\n');
 }
 }
 else
-{
 putchar('\n');
-}
 }
