a0e61548ea5b079418d51e763ea4c3dad5787ea8
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-07-24 13:38:43 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,6 @@
  */
 void puts2(char *str)
 {
-void puts2(char *str)
-{
 int len, i;
 len = 0;
 while (str[len] != '\0')
@@ -22,5 +20,4 @@ for (i = 0; i < len; i += 2)
 putchar(str[i]);
 }
 putchar('\n');
-}
 }
