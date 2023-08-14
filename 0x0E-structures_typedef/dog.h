b0e142ff603c1b01e82de167c71ea6e2eac45344
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-08-14 12:52:17 -0700

    dog h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
+#ifndef s
+#define s
 #include <stdio.h>
 
-struct dog {
+/**
+ * struct dog - defines a group of variables
+ * Retur: 0
+ */
+
+struct dog
+{
 char *name;
 float age;
 char *owner;
