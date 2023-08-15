c51c88438aae6b0d79f155ed3f1c4223038fbba8
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-08-14 20:04:07 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,9 +5,7 @@
 /**
  * print_dog - intialize function
  * @d: pointer
- * @name: char 1
- * @age: float 1
- * @owner: char 2
+ * Return: Nothing
  */
 
 void print_dog(struct dog *d)
@@ -15,7 +13,7 @@ void print_dog(struct dog *d)
 if (d != NULL)
 {
 printf("name: %s\n", d->name ? d->name : "(nil)");
-printf("age: %f\n", d->age ? d->age : "(nill)");
+printf("age: %f\n", d->age ? d->age : 0);
 printf("owner: %s\n", d->owner ? d->owner : "(nil)");
 }
 }
