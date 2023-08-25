114782f7f1220781fcf5149361f3cb2ffc5e2ab6
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-08-24 17:46:49 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * size_t print_list - function that prints all the elements of list
+ * @h: pointer
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+size_t i;
+for (i = 0; h; i++)
+{
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+}
+return (i);
+}
